fe536194a21c7c04ff01e7a8faf0616e194f3c68
taeyoungNew <dndbxhd10@naver.com>
2023-05-29 18:21:12 +0900

    변경사항 20230529

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const API_URL = 'https://api.themoviedb.org/3/'
-const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p'
 
+const top_rated ='top_rated'
+const popular ='popular'
+const now_playing ='now_playing'
+const upcoming ='upcoming'
 
-function getMovies() {
-  console.log('getMovies')
+let pageCnt = 1;
+let movie_type = '';
+function getRatedMovies(param) {
+  movie_type = param
+  if(movie_type != param) {
+    console.log('다른 영화 검색')
+  }
+  
+  let totalPage = 1;
+  // console.log('getMovies')
   const options = {
     method: 'GET',
     headers: {
@@ -12,21 +24,40 @@ function getMovies() {
     }
   };
   
-  fetch(`${API_URL}movie/top_rated?language=en-US&page=1 `, options)
+  fetch(`${API_URL}movie/${movie_type}?language=en-US&page=${pageCnt} `, options)
     .then(response => response.json())
     .then(response => {
+      console.log(response);
+      totalPage = 556;
+      // 받아온 api를 변수에 담기
       const movieDatas = response.results;
-      console.log('movieList = ', movieDatas);
-      // const movieList = document.querySelector('#movie-card-list');
-      // let newCard = `<div class="item-box">
-      //                 <div class="item">
-      //                   <div class="front">
-      //                     <img src="https://yt3.googleusercontent.com/LGPDh1G3fhM4yf6gLyigaMyeaVbzmRLL-iUGXYvDWWYudTauEjAJUKkGB064H-pVi2uHqbM_qsw=s900-c-k-c0x00ffffff-no-rj" alt="">
-      //                   </div>
-      //                   <div class="back">
-      //                   </div>
-      //                 </div>
-      //               </div>`
+      
+      // movieList 접근    
+      const movieList = document.querySelector('#movie-card-list');
+      // console.log('movieList = ', movieDatas);
+      movieDatas.map((val) => {
+        console.log(val)
+        const temp = document.createElement("div");
+        // HTML요소 추가하기
+        temp.innerHTML = `<div class="item">
+                            <div class="back" style=" background-size: cover; background-position: center;  background-image: URL('${IMAGE_BASE_URL}/original${val.poster_path}')">
+                              <div class="movie-info">
+                                <h3>${val.title}</h3>
+                                <h5>release_date : ${val.release_date}</h5>
+                                <h5>grade: ${val.vote_average}</h5>
+                                <p>${val.overview}</p>
+                              </div>
+                            </div>
+                            <div class="front">
+                              <img src="${IMAGE_BASE_URL}/original${val.poster_path}" alt="">
+                            </div>
+                          </div>`
+        movieList.append(temp)
+        pageCnt++;
+      })
+      
+      
+      
     })
     .catch(err => console.error(err));
   
