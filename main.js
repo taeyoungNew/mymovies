729fb8fdceab8de48c55f9548966c8b66e1b85b1
taeyoungNew <dndbxhd10@naver.com>
2023-05-29 15:36:17 +0900

    변경사항 20230529

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const API_URL = 'https://api.themoviedb.org/3/'
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
 
+
 function getMovies() {
   console.log('getMovies')
   const options = {
@@ -13,7 +14,20 @@ function getMovies() {
   
   fetch(`${API_URL}movie/top_rated?language=en-US&page=1 `, options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => {
+      const movieDatas = response.results;
+      console.log('movieList = ', movieDatas);
+      // const movieList = document.querySelector('#movie-card-list');
+      // let newCard = `<div class="item-box">
+      //                 <div class="item">
+      //                   <div class="front">
+      //                     <img src="https://yt3.googleusercontent.com/LGPDh1G3fhM4yf6gLyigaMyeaVbzmRLL-iUGXYvDWWYudTauEjAJUKkGB064H-pVi2uHqbM_qsw=s900-c-k-c0x00ffffff-no-rj" alt="">
+      //                   </div>
+      //                   <div class="back">
+      //                   </div>
+      //                 </div>
+      //               </div>`
+    })
     .catch(err => console.error(err));
   
 }
